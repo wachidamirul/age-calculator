a22607517f440211bb92d9d1e958dd3b9eda12f7
Wachid Amirul M <wachidamirul@gmail.com>
2024-07-23 19:52:56 +0700

    feat: assets  model

---
+++ src/js/assets.js
@@ -0,0 +1,68 @@
+const assets = [
+  {
+    name: "young-adult",
+    image: "./assets/models/young-adult.gif",
+    style:
+      "width: 245px; height: auto; z-index: 20; position: absolute; bottom: 65px;",
+  },
+  {
+    name: "very-old",
+    image: "./assets/models/very-old.gif",
+    style:
+      "width: 170px; height: auto; z-index: 20; position: absolute; bottom: 65px;",
+  },
+  {
+    name: "toddler",
+    image: "./assets/models/toddler.gif",
+    style:
+      "width: 170px; height: auto; z-index: 20; position: absolute; bottom: 120px;",
+  },
+  {
+    name: "teenager",
+    image: "./assets/models/teenager.gif",
+    style:
+      "width: 370px; height: auto; z-index: 20; position: absolute; bottom: 40px;",
+  },
+  {
+    name: "rip",
+    image: "./assets/models/rip.gif",
+    style:
+      "width: 240px; height: auto; z-index: 20; position: absolute; bottom: 20px;",
+  },
+  {
+    name: "preschooler",
+    image: "./assets/models/preschooler.gif",
+    style:
+      "width: 190px; height: auto; z-index: 20; position: absolute; bottom: 140px;",
+  },
+  {
+    name: "old",
+    image: "./assets/models/old.gif",
+    style:
+      "width: 375px; height: auto; z-index: 20; position: absolute; bottom: 40px;",
+  },
+  {
+    name: "elementary",
+    image: "./assets/models/elementary.gif",
+    style:
+      "width: 300px; height: auto; z-index: 20; position: absolute; bottom: 40px;",
+  },
+  {
+    name: "cat",
+    image: "./assets/models/cat.gif",
+    style:
+      "width: 250px; height: auto; z-index: 20; position: absolute; bottom: 120px;",
+  },
+  {
+    name: "baby",
+    image: "./assets/models/baby.gif",
+    style:
+      "width: 250px; height: auto; z-index: 20; position: absolute; bottom: 80px;",
+  },
+  {
+    name: "adult",
+    image: "./assets/models/adult.gif",
+    style:
+      "width: 370px; height: auto; z-index: 20; position: absolute; bottom: 65px;",
+  },
+];
